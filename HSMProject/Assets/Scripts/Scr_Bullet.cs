4d9aa764bf6d59f8bc3cd40d9c00a9c01e26d195
AntoVKNG <antonin.druelle@gmail.com>
2019-04-02 15:09:14 +0200

    Début de tour Alchimie

--- HSMProject/Assets/Scripts/Scr_Bullet.cs
+++ HSMProject/Assets/Scripts/Scr_Bullet.cs
@@ -8,14 +8,14 @@ public class Scr_Bullet : MonoBehaviour
     public float speed = 70f;
     public static int attackDamageMin = 4;
     public static int attackDamageMax = 6;
-    //ennemy health;
-    GameObject Ennemy;
+    //Scr_Enemy health;
+    GameObject Scr_Enemy;
 
 
     private void Awake()
     {
-        Ennemy = GameObject.FindGameObjectWithTag("Enemy");
-        //health = GetComponent<ennemy>();
+        Scr_Enemy = GameObject.FindGameObjectWithTag("Enemy");
+        //health = GetComponent<Scr_Enemy>();
     }
 
     public void Seek (Transform _target)
--- HSMProject/Assets/Scripts/turretB.cs
+++ HSMProject/Assets/Scripts/turretB.cs
@@ -6,9 +6,9 @@ using UnityEngine.UI;
 public class turretB : MonoBehaviour {
 
     private Transform target;
-    private ennemy targetEnemy;
+    private Scr_Enemy targetEnemy;
     public float range;
-    public string enemyTag = "Ennemy";
+    public string enemyTag = "Scr_Enemy";
     public Transform partToRotate;
     public float turnSpeed = 10f;
     public float fireRate = 1f;
@@ -97,7 +97,7 @@ public class turretB : MonoBehaviour {
             
             nearestEnemy = other.gameObject;
             target = nearestEnemy.transform;
-            targetEnemy = nearestEnemy.GetComponent<ennemy>();
+            targetEnemy = nearestEnemy.GetComponent<Scr_Enemy>();
 
             Debug.Log("Il tire !");
 
