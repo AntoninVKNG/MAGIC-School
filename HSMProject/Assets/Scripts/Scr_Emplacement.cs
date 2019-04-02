112cc5195723ff92d22792919c1ab3fdea99ae6f
DESMARCHELIER Mathieu <m.desmarchelier@rubika-edu.com>
2019-04-02 15:07:37 +0200

    Student Upgrade not finish

--- HSMProject/Assets/Scripts/Scr_Emplacement.cs
+++ HSMProject/Assets/Scripts/Scr_Emplacement.cs
@@ -6,10 +6,9 @@ public class Scr_Emplacement : MonoBehaviour
 {
     public static Scr_Emplacement Scr_Emplacement_static;
     public static Vector3 positionnement;
-    public  int fire = 0;
-    public  int alchimie = 0;
-    public  int explosion = 0;
-    public  int invocation = 0;
+    public  int niveau = 0; //niveau de 0-16
+
+
 
     
     void Start()
@@ -27,9 +26,9 @@ public class Scr_Emplacement : MonoBehaviour
         Scr_Student.pos = this.transform.position;
         Scr_Student.Scr_StudentStatic.Canvas.transform.position = positionnement;
         Scr_Student.Scr_StudentStatic.Canvas.transform.position = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
-        Scr_Student.Scr_StudentStatic.SwitchCanvasApparition();
-        
-        Debug.Log("switch");
+        // Scr_Student.Scr_StudentStatic.SwitchCanvasApparition();
+        //Scr_Student.Scr_StudentStatic.DetectionLevel();
+       // Debug.Log("switch");
 
 
     }
